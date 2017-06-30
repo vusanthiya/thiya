a0ccabe4706e3d73e5b8daa5b8800b73bbd3d8a5
vusanthiya <vusanthiya@gmail.com>
2017-06-30 10:48:11 +0530

    Create abb.c

---
+++ abb.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x,y,z;
+clrscr();
+printf("\n Enter the three numbers:");
+scanf("%d%d%d",&x,&y,&z);
+if((a>b)&&(a>c))
+printf("\n a is greater");
+else if((b>a)&&(b>c))
+printf("\n b is greater");
+else
+printf("\n c is greater");
+getch();
+}
