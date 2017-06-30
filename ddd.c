fc9a990086278801edaa39753142ef0fca9d6499
vusanthiya <vusanthiya@gmail.com>
2017-06-30 10:56:53 +0530

    Update ddd.c

--- ddd.c
+++ ddd.c
@@ -6,7 +6,7 @@ int a;
 clrscr();
 printf("\n Enter the year:");
 scanf("%d",&a);
-if((a%4==0 && a%100==0 && a%400==0)||(a%4==0 && a%400==0))
+if((a%4==0 && a%100==0 && a%400==0)||(a%4==0 && a%100==0))
 printf("\n leap year");
 else
 printf("\n Not a leap year");
