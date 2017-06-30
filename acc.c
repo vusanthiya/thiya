03a049a9e92aa73a367c4fe6366f781be653d730
vusanthiya <vusanthiya@gmail.com>
2017-06-30 10:41:47 +0530

    Create acc.c

---
+++ acc.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char word;
+clrscr();
+printf("\n Enter the word:");
+scanf("%c",&word);
+if(word=='a'||word=='e'||word=='i'||word=='o'||word=='u')
+printf("\n vowel);
+else
+printf("\n Consonant);
+getch();
+}
